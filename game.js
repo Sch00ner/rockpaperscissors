71e960ed103cf297b852c186b862e48291ca2d83
Sounak Roy <roysounak@protonmail.com>
2022-06-24 18:03:12 +0530

    Figures out who won

--- game.js
+++ game.js
@@ -2,3 +2,35 @@ function computerPlay() {
     let item = ["rock", "paper", "scissors"];
     return item[Math.floor(Math.random() * (item.length - 1))]
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase()
+
+    if(computerSelection == playerSelection) {
+        console.log("Aw shucks! That's a draw.")
+    }
+    else if(playerSelection == "rock") {
+        if(computerSelection == "scissors") {
+            console.log("You Win! Rock beats Scissors.")
+        }       
+        else {
+            console.log("You Lose! Paper beats Rock.")
+        }
+    }
+    else if(playerSelection == "scissors") {
+        if(computerSelection == "paper") {
+            console.log("You Win! Scissors beats Paper.")
+        }
+        else {
+            console.log("You Lose! Rock beats Scissors.")
+        }
+    }
+    else {
+        if(computerSelection == "rock") {
+            console.log("You Win! Paper beats Rock.")
+        }
+        else {
+            console.log("You Lose! Scissors beats Paper.")
+        }
+    }
+}
