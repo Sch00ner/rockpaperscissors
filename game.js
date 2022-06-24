18e497541fed46b36f68c4653f97b6fe0d0646bd
Sounak Roy <roysounak@protonmail.com>
2022-06-24 18:06:56 +0530

    Added a test case

--- game.js
+++ game.js
@@ -4,33 +4,38 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    
     playerSelection = playerSelection.toLowerCase()
 
     if(computerSelection == playerSelection) {
-        console.log("Aw shucks! That's a draw.")
+        console.log("Aw shucks! That's a draw.");
     }
     else if(playerSelection == "rock") {
         if(computerSelection == "scissors") {
-            console.log("You Win! Rock beats Scissors.")
+            console.log("You Win! Rock beats Scissors.");
         }       
         else {
-            console.log("You Lose! Paper beats Rock.")
+            console.log("You Lose! Paper beats Rock.");
         }
     }
     else if(playerSelection == "scissors") {
         if(computerSelection == "paper") {
-            console.log("You Win! Scissors beats Paper.")
+            console.log("You Win! Scissors beats Paper.");
         }
         else {
-            console.log("You Lose! Rock beats Scissors.")
+            console.log("You Lose! Rock beats Scissors.");
         }
     }
     else {
         if(computerSelection == "rock") {
-            console.log("You Win! Paper beats Rock.")
+            console.log("You Win! Paper beats Rock.");
         }
         else {
-            console.log("You Lose! Scissors beats Paper.")
+            console.log("You Lose! Scissors beats Paper.");
         }
     }
 }
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
